3c9f4da04f72852d7b2347a58de3efc503a38a9b
gwpmad <gmaddocks1@gmail.com>
2020-05-07 15:33:03 +0100

    Border radius on new game button

--- src/components/new-game-button/container.js
+++ src/components/new-game-button/container.js
@@ -4,6 +4,7 @@ const Container = styled.button`
   ${({ theme: { colors } }) => css`
     background-color: white;
     border: 3px solid ${colors.lightBlack};
+    border-radius: 4px;
     color: ${colors.lightBlack};
     cursor: pointer;
     font-size: 25px;
