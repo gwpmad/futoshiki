3fcf37c31d7ba32c0666fd828c5d68c4f4ac2b59
gwpmad <gmaddocks1@gmail.com>
2020-05-14 15:00:42 +0100

    Updates

---
+++ precache-manifest.7650c5a19c0d2b5405a30d259cafdd41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504efa8361b5d0870d319f5146d4d5c6",
+    "url": "/futoshiki/index.html"
+  },
+  {
+    "revision": "219123318616718da632",
+    "url": "/futoshiki/static/js/2.5b1837dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/futoshiki/static/js/2.5b1837dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a7f1c4dbacada453cb",
+    "url": "/futoshiki/static/js/main.b617fc42.chunk.js"
+  },
+  {
+    "revision": "419f7761cf3be6ec334d",
+    "url": "/futoshiki/static/js/runtime-main.1a0ab4e7.js"
+  }
+]);
