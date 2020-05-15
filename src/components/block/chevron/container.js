1c6115f9f4a7b30c56c633accaa1b1a6495b1d70
gwpmad <gmaddocks1@gmail.com>
2020-05-15 14:06:00 +0100

    Fix Firefox chevron positioning

--- src/components/block/chevron/container.js
+++ src/components/block/chevron/container.js
@@ -3,16 +3,28 @@ import styled, { css } from 'styled-components';
 function getDirectionStyles(direction) {
   switch (direction) {
     case 'right':
-      return `left: 114%;`;
+      return `left: 114%;
+      @-moz-document url-prefix() {
+        bottom: 21%;
+      }`;
     case 'left':
       return `right: 114%;
-      transform: rotate(180deg);`;
+      transform: rotate(180deg);
+      @-moz-document url-prefix() {
+        bottom: 10%;
+      }`;
     case 'above':
       return `transform: rotate(270deg);
-      bottom: 98%;`;
+      bottom: 98%;
+      @-moz-document url-prefix() {
+        left: 29%;
+      }`;
     case 'below':
       return `transform: rotate(90deg);
-      top: 98%;`;
+      top: 98%;
+      @-moz-document url-prefix() {
+        right: 29%;
+      }`;
     default:
       return '';
   }
